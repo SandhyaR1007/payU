d005ca0d3ee693e1bc4a181592002fc962812624
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-24 15:21:18 +0530

    add: auth middleware

---
+++ backend/routes/middleware.js
@@ -0,0 +1,23 @@
+const { JWT_TOKEN } = require("../config");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  let authToken = req.headers.authorization;
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  authToken = authToken.split(" ")[1];
+  try {
+    const decoded = jwt.verify(authToken, JWT_TOKEN);
+    if (decoded.userId) {
+      req.userId = decoded.userId;
+      next();
+    } else {
+      res.status(401).json({ message: "Unauthorized access" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized access" });
+  }
+};
+
+module.exports = { authMiddleware };
