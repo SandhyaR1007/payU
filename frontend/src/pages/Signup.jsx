b2b7fce25dfe5b9fc0917354d7c1ac3139fee51a
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-25 19:18:10 +0530

    update: signup a user

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,83 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import InputBox from "../components/Inputbox";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [formInputs, setFormInputs] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormInputs({ ...formInputs, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/api/v1/user/signup", formInputs)
+      .then(function (response) {
+        const token = response.data.token;
+        if (token) {
+          sessionStorage.setItem("token", token);
+          return navigate("/dashboard");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="max-w-sm m-auto mt-20 p-10 flex flex-col  gap-3 shadow-custom rounded-md">
+      <header className="text-center">
+        <Heading heading="Sign Up" />
+        <SubHeading text="Enter your information to create an account" />
+      </header>
+      <main>
+        <form action="" className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          <InputBox
+            label="First Name"
+            name="firstname"
+            handleChange={handleChange}
+          />
+          <InputBox
+            label="Last Name"
+            name="lastname"
+            handleChange={handleChange}
+          />
+          <InputBox
+            label="Email"
+            name="username"
+            type="email"
+            handleChange={handleChange}
+          />
+          <InputBox
+            label="Password"
+            name="password"
+            type="password"
+            handleChange={handleChange}
+          />
+          <button className="bg-gray-900 text-white p-3 rounded-md text-sm">
+            Sign up
+          </button>
+        </form>
+      </main>
+      <footer className="text-center">
+        <p>
+          Already have an account?{" "}
+          <Link to="/signin" className="hover:underline">
+            Login
+          </Link>
+        </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Signup;
