607160178209fe77a4e90b1da2de555185dea9f8
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-26 22:08:48 +0530

    add: custom hook for localstorage

---
+++ frontend/src/hooks/useLocalStorage.jsx
@@ -0,0 +1,9 @@
+export const useLocalStorage = () => {
+  const setLocalStorage = (key, value) => {
+    localStorage.setItem(key, value);
+  };
+  const getLocalStorage = (key) => {
+    return localStorage.getItem(key);
+  };
+  return { setLocalStorage, getLocalStorage };
+};
