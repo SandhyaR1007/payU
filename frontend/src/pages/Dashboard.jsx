ba69f0c66d984dd8d9908db517ce000dc57375a0
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-26 22:09:33 +0530

    update: display user listing

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import Heading from "../components/Heading";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { getAllUsers } from "../apis/apiUtils";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [users, setUsers] = useState([]);
+  const { getLocalStorage } = useLocalStorage();
+  useEffect(() => {
+    const token = getLocalStorage("token");
+    (async () => {
+      try {
+        const data = await getAllUsers(token);
+        if (data) {
+          setUsers(data.data.users);
+        }
+      } catch (err) {
+        console.log("Error occured when fetching users");
+      }
+    })();
+
+    return () => {};
+  }, []);
+  return (
+    <div>
+      <nav>
+        <Heading heading="Payments App" />
+        <div>Profile</div>
+      </nav>
+      <main>
+        <section>Your Balance</section>
+        <section>
+          <h3>Users</h3>
+          <div>
+            {users.map((user) => (
+              <p key={user._id}>{user.firstname}</p>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
