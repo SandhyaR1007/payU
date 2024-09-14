577c38ca28bea5fcfbc5afa716de89a35ead94c2
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-09-14 22:05:32 +0530

    update: add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,47 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import { useEffect } from "react";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route
+          path="/signin"
+          element={
+            <AuthRedirect>
+              <Signin />
+            </AuthRedirect>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthRedirect>
+              <Signup />
+            </AuthRedirect>
+          }
+        />
       </Routes>
     </>
   );
 };
 
 export default App;
+
+const PrivateRoutes = () => {
+  let isAuthenticated = localStorage.getItem("token");
+  return isAuthenticated ? <Outlet /> : <Navigate to="/signin" />;
+};
+const AuthRedirect = ({ children }) => {
+  let isAuthenticated = localStorage.getItem("token");
+
+  return isAuthenticated ? <Navigate to="/dashboard" /> : children;
+};
